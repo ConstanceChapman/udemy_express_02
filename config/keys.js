720aeb0385c03708575bbe65a63b87c7a96e1b7b
Constance Chapman <constancealicechapman224@gmail.com>
2018-10-15 16:45:51 +0100

    handled auth

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI: 'mongodb://connie:connie1@ds131983.mlab.com:31983/udemy-storybooks-dev',
+  googleClientID: '589767015913-g4uul0hskurcpmu164dsisq8omgprbaq.apps.googleusercontent.com',
+  googleClientSecret: '8gjuMllh_ihligTjWySQzut_'
+}
